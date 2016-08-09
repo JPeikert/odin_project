539eddb2c428d7dff4192a4af3a6e3d2e668e343
JPeikert <jpeikert@gmail.com>
2016-08-09 13:38:40 +0200

    Add substrings project

---
+++ ruby/building_blocks/substrings/substrings.rb
@@ -0,0 +1,25 @@
+def substrings(string, dictionary)
+  matches = Hash.new
+  string.downcase!
+
+  dictionary.each do |word|
+    string.scan(word.downcase) do |match|
+      matches.key?(match) ? matches[match] += 1 : matches[match] = 1
+    end
+  end
+
+  matches.each do |key, value|
+    print "#{value} - #{key}\n"
+  end
+end
+
+
+
+puts "Your dictionary:"
+dictionary = gets.split(/[,\s]+/).map {|x| x}
+puts "Your string:"
+string = gets.chomp
+puts "Your dictionary: #{dictionary}"
+puts "Your string: #{string}"
+
+substrings(string, dictionary)
