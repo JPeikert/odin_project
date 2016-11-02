ed2490a69c4ce59020cfd7f39459c96590283fda
JPeikert <jpeikert@gmail.com>
2016-11-02 16:05:45 +0100

    Add EventManager project

---
+++ ruby/serialization/event_manager/README.md
@@ -0,0 +1,22 @@
+#Event Manager
+
+Implementation of an exercise from [The Odin Project](http://theodinproject.com)'s  [*Project: Serialization and Working with Files*](http://www.theodinproject.com/ruby-programming/file-i-o-and-serialization)
+
+Full tutorial at [jumpstart labs](http://tutorials.jumpstartlab.com/projects/eventmanager.html)
+
+Program parses .csv file, standardizes records, creates new files using ERB template and calculates peak registrations hours and days.
+
+####The techniques practiced:
+
+* File input and output
+* Reading data from CSV file
+* String manipulation
+* Using Date library
+* Accessing Sunlight’s Congressional API through the Sunlight Congress gem
+* Using ERB for templating
+
+####Requirements
+
+* Ruby
+* sample data in CSV format
+* Sunlight-Congress gem
--- ruby/serialization/event_manager/lib/event_manager.rb
+++ ruby/serialization/event_manager/lib/event_manager.rb
@@ -1,28 +1,92 @@
 require "csv"
 require "sunlight/congress"
+require "erb"
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
 
 def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5, "0")[0..4]
 end
 
-puts "EventManager initialized.\n\n"
+def clean_phone_number(phone_number)
+  phone_number.to_s.gsub!(/\D/, '')
+
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number == 11 && phone_number[0] == 1
+    phone_number[1..10]
+  else
+    "0"*10
+  end
+end
+
+def legislators_by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir("output") unless Dir.exist?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') { |file| file.puts form_letter }
+end
+
+def popular_hours(registration_dates)
+  hours = Hash.new(0)
+
+  registration_dates.each do |regdate|
+    date = DateTime.strptime(regdate, "%m/%d/%y %H:%M")
+    hours[date.strftime("%H")] += 1
+  end
+
+  hours = hours.sort_by {|key, value| value }.reverse
+
+  puts "Popularity of hours"
+  hours.each do |hour|
+    puts "#{hour[0]}: #{hour[1]}"
+  end
+end
+
+def popular_days(registration_dates)
+  days = Hash.new(0)
+
+  registration_dates.each do |regdate|
+    date = DateTime.strptime(regdate, "%m/%d/%y %H:%M")
+    days[date.strftime("%A")] += 1
+  end
+
+  days = days.sort_by {|key, value| value }.reverse
+
+  puts "Popularity of week days"
+  days.each do |day|
+    puts "#{day[0]}: #{day[1]}"
+  end
+end
+
+puts "EventManager initialized."
 
 contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
 
+template_letter = File.read("form_letter.erb")
+erb_template = ERB.new(template_letter)
+registration_dates = []
+
 contents.each do |row|
+  id = row[0]
 	name = row[:first_name]
 	zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:homephone])
+  registration_dates << row[:regdate]
+	legislators = legislators_by_zipcode(zipcode)
 
-	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
 	
-	puts "#{name} - #{zipcode} - #{legislators}"
+  save_thank_you_letters(id, form_letter)
 end
-# lines = File.exist?("event_attendees.csv") ? File.readlines("event_attendees.csv") : "Error - file not found"
 
-# lines.each_with_index do |line, index|
-# 	next if index == 0
-#   columns = line.split(",")
-#   puts columns[2]
-# end
+puts "Letters saved"
+
+popular_hours(registration_dates)
+popular_days(registration_dates)
