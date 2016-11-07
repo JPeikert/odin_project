ce2fb5b4f12f5e32ee8fd3c644bc5dcae40f80a9
JPeikert <jpeikert@gmail.com>
2016-11-07 16:39:20 +0100

    I3 Tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,19 @@
 class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect { |tag| tag.strip.downcase }.uniq
+    tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = tags
+  end
 
 end
